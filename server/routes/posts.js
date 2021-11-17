eb9762d1115d8cbc74616000586c8938f928ee5f
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-11-17 13:22:08 +0100

    post route

---
+++ server/routes/posts.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+// import User from "../models/User";
+import Post, { findById, findByIdAndUpdate, find } from "../models/Post";
+
+//CREATE POST
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE POST
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can update only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET POST
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await find({ username });
+    } else if (catName) {
+      posts = await find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await find();
+    }
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;
